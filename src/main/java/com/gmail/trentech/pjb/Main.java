43c76f50c742b112014194cea601a6b651ae8040
TrenTech <TrenTech@gmail.com>
2016-10-19 03:14:55 -0400

    dependencies

--- src/main/java/com/gmail/trentech/pjb/Main.java
+++ src/main/java/com/gmail/trentech/pjb/Main.java
@@ -17,7 +17,7 @@ import com.google.inject.Inject;
 import me.flibio.updatifier.Updatifier;
 
 @Updatifier(repoName = Resource.NAME, repoOwner = Resource.AUTHOR, version = Resource.VERSION)
-@Plugin(id = Resource.ID, name = Resource.NAME, version = Resource.VERSION, description = Resource.DESCRIPTION, authors = Resource.AUTHOR, url = Resource.URL, dependencies = { @Dependency(id = "Updatifier", optional = true) })
+@Plugin(id = Resource.ID, name = Resource.NAME, version = Resource.VERSION, description = Resource.DESCRIPTION, authors = Resource.AUTHOR, url = Resource.URL, dependencies = { @Dependency(id = "Updatifier", optional = true), @Dependency(id = "helpme", optional = true) })
 public class Main {
 
 	@Inject
