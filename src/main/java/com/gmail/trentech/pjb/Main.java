f1782711f38bff4cc2d804f011817494e1298ce3
TrenTech <TrenTech@gmail.com>
2017-08-21 11:37:12 -0400

    target correct pjc version

--- src/main/java/com/gmail/trentech/pjb/Main.java
+++ src/main/java/com/gmail/trentech/pjb/Main.java
@@ -14,10 +14,7 @@ import com.gmail.trentech.pjb.init.Common;
 import com.gmail.trentech.pjb.utils.Resource;
 import com.google.inject.Inject;
 
-import me.flibio.updatifier.Updatifier;
-
-@Updatifier(repoName = Resource.NAME, repoOwner = Resource.AUTHOR, version = Resource.VERSION)
-@Plugin(id = Resource.ID, name = Resource.NAME, version = Resource.VERSION, description = Resource.DESCRIPTION, authors = Resource.AUTHOR, url = Resource.URL, dependencies = { @Dependency(id = "Updatifier", optional = true), @Dependency(id = "pjc", optional = false) })
+@Plugin(id = Resource.ID, name = Resource.NAME, version = Resource.VERSION, description = Resource.DESCRIPTION, authors = Resource.AUTHOR, url = Resource.URL, dependencies = { @Dependency(id = "pjc", optional = false) })
 public class Main {
 
 	@Inject
